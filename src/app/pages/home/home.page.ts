f8e65cdb59e791158e5081361e015fa1f0de3135
vanutcho123 <hainguyen.061001@gmail.com>
2023-11-07 10:36:59 +0700

    Call api

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Result } from 'src/app/interfaces/movieNowPlaying';
+import { MovieService } from 'src/app/services/movie/movie.service';
 import { homeData } from 'src/app/utils/fetchData';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,23 @@ import { homeData } from 'src/app/utils/fetchData';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  constructor() {}
-  videos: any = [];
+  constructor(private movieService: MovieService) {}
+  public dataMovieNowPlaying: Result[] = [];
+  public baseImageUrl: string = '';
   ngOnInit() {
-    this.videos = homeData;
+    this.baseImageUrl = environment.imageUrl;
+    this.getDataMovieNowPlaying();
+  }
+
+  getDataMovieNowPlaying() {
+    this.movieService.getMovieNowPlaying().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.dataMovieNowPlaying = res.results;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { RootObject } from 'src/app/interfaces/movieNowPlaying';
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
   constructor(private httpClient: HttpClient) {}
+  getMovieNowPlaying(page = 1): Observable<RootObject> {
+    return this.httpClient.get<RootObject>(
+      `${environment.bareUrl}/movie/now_playing?api_key=${environment.apiKey}&page=${page}`,
+      {
+        headers: {
+          Authorization: environment.accessToken,
+        },
+      }
+    );
+  }
 }
